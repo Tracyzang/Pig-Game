2d1a89426ad58ef22f0923b5bba4c0b8d13d24ca
Tracy(Zhiqiu) Zang <40729720+Tracyzang@users.noreply.github.com>
2018-12-30 16:43:51 -0800

    add hold button

--- starter/app.js
+++ starter/app.js
@@ -31,4 +31,47 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.src = "dice-" + dice + ".png";
 
   //3.Update the round score if the rolled number was not a 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //Add current score to global score
+  scores[activePlayer] += roundScore;
+  //Update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  //Check if player won the game
+  if (scores[activePlayer] >= 100) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  //next player
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
